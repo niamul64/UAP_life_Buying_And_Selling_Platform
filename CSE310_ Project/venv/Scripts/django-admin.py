39fa84f9311c9496b62e3ef6e110e38f4dca73c6
Md.Anik Khan <43582903+CodeAnik@users.noreply.github.com>
2019-12-17 13:28:20 +0600

    included HTML file for home page

---
+++ CSE310_ Project/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Plab5 Pc27\Documents\GitHub\UAP_life_Buying_And_Selling_Platform\CSE310_ Project\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
