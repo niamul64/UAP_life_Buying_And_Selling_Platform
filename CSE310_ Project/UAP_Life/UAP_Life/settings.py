0ef25d40648cb5238199f74ec262045188f408d5
Niamul Hasan <48917799+niamul64@users.noreply.github.com>
2019-12-18 00:49:19 +0600

    Home_Page has set.

--- CSE310_ Project/UAP_Life/UAP_Life/settings.py
+++ CSE310_ Project/UAP_Life/UAP_Life/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'UAP_Life.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
