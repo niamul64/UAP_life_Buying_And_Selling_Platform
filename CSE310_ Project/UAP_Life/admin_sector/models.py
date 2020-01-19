46694be1d8055aefaa1ec1214ec0cb10175a366a
Md.Anik Khan <anikse111@gmail.com>
2020-01-20 00:25:20 +0600

    Models Uapdate
    just Admin_sector,Post_Ad,User_profile Models done.

--- CSE310_ Project/UAP_Life/admin_sector/models.py
+++ CSE310_ Project/UAP_Life/admin_sector/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class admin_sector(models.model):
+    name = models.CharField(max_length=100, default="")
+
+    def __str__(self):
+        return self.name
