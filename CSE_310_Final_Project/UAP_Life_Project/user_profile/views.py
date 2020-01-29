264d11f30457f88f2b440bcb991c1ee8e7232e57
mksaadi <mksaadi03@gmail.com>
2020-01-30 00:55:17 +0600

    log in + log out
    complete for now... custom validation for our uapmail has to be added... but just basic  stuff for now...

--- CSE_310_Final_Project/UAP_Life_Project/user_profile/views.py
+++ CSE_310_Final_Project/UAP_Life_Project/user_profile/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from user_profile.forms import UserForm, UserProfileInfoForm
+from django.contrib.auth import authenticate, logout, login
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
 
-
+@login_required
 def home(request):
     return render(request, 'user_profile/Main_page.html')
 
 
 def index(request):
     return render(request, 'user_profile/index.html')
+
+
+def loginpage(request):
+    return render(request, 'user_profile/login.html')
 
 
 def signup(request):
@@ -17,11 +24,11 @@ def signup(request):
         profile_form = UserProfileInfoForm(data=request.POST)
 
         if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
+            user = user_form.save()                 # saving to database
+            user.set_password(user.password)        # hashing the password
             user.save()
             profile = profile_form.save(commit=False)
-            profile.user = user
+            profile.user = user                     # one to one relationship
 
             if 'profile_pic' in request.FILES:
                 profile.profile_pic = request.FILES['profile_pic']
@@ -39,6 +46,29 @@ def signup(request):
                             'profile_form': profile_form,
                             'registered': registered})
 
+# log in view
 
-def login(request):
-    return render(request, 'user_profile/login.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        given_username = request.POST.get('username')
+        given_password = request.POST.get('password')
+        user = authenticate(username=given_username, password=given_password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return render(request, 'user_profile/Main_page.html')
+            else:
+                return HttpResponse('Account not active!!')
+        else:
+            return HttpResponse("Invalid login information!!")
+
+    else:
+        return render(request, 'user_profile/login.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return render(request, 'user_profile/index.html')
